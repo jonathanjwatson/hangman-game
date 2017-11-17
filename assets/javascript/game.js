465250f162bedd8293f7211f8b58e48687ce0b6e
jwats287 <jwats287@users.noreply.github.com>
2017-11-17 17:10:01 -0500

    Game logic works to display correct letters on the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -46,23 +46,51 @@
 
 var playersGuessedLetter = ""
 var wordToGuess = "madonna"
+wordToGuess = wordToGuess.split("")
+var answerArray = [];
+
+var fillTheAnswerArray = () => {
+    for (var i = 0; i < wordToGuess.length; i++) {
+        answerArray.push("___");
+    }
+}
+
+var createTheAnswerHTML = () => {
+    var htmlToDisplay = "";
+    for (var i = 0; i < answerArray.length; i++) {
+        htmlToDisplay += `<span id=${i}> ${answerArray[i]} </span>`
+    }
+    $("#word").append(htmlToDisplay);
+    htmlToDisplay = "";
+}
+
+var updateSpecificLetter = (number, correctLetter) => {
+    var span = document.getElementById(`${number}`);
+    span.innerHTML = correctLetter;
+}
 
 $(document).ready(function() {
     // console.log("Document is ready")
+    fillTheAnswerArray();
+    createTheAnswerHTML();
     
-    for (var i = 0; i < wordToGuess.length; i++) {
-        // $("#word").append("<span id=>_ </span>");
-        $("#word").append(`<span id=${i}> _ </span>`)
-    }
 })
 
 document.onkeyup = function(e) {
     // console.log(e);
     typedLetter = e.key.toLowerCase();
-    playersGuessedLetter = typedLetter;
-    console.log(playersGuessedLetter)
-    var lettersPositionInTheArray = wordToGuess.indexOf(playersGuessedLetter)
-    console.log(lettersPositionInTheArray)
+    console.log(typedLetter)
+    for (var i = 0; i < wordToGuess.length; i++) {
+        if (typedLetter === wordToGuess[i]) {
+            answerArray[i] = typedLetter;
+            console.log(typedLetter)
+            console.log(answerArray);
+            updateSpecificLetter(i, typedLetter);
+        }
+    
+    }
+
+    
 }
 
 
