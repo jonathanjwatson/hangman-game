6a2a65cdc291932638c017874dd8530bb930e827
jwats287 <jwats287@users.noreply.github.com>
2017-11-17 16:42:21 -0500

    roughed out game js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,68 @@
-console.log("This is a test")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var playersGuessedLetter = ""
+var wordToGuess = "madonna"
+
+$(document).ready(function() {
+    // console.log("Document is ready")
+    
+    for (var i = 0; i < wordToGuess.length; i++) {
+        // $("#word").append("<span id=>_ </span>");
+        $("#word").append(`<span id=${i}> _ </span>`)
+    }
+})
+
+document.onkeyup = function(e) {
+    // console.log(e);
+    typedLetter = e.key.toLowerCase();
+    playersGuessedLetter = typedLetter;
+    console.log(playersGuessedLetter)
+    var lettersPositionInTheArray = wordToGuess.indexOf(playersGuessedLetter)
+    console.log(lettersPositionInTheArray)
+}
+
+
