b572a6cff000418444acfeb13d1ea4207733aa39
jwats287 <jwats287@users.noreply.github.com>
2017-11-20 10:27:27 -0500

    working on randomly selecting a word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,21 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var wordBank = ["Apple", "Orange", "Banana", "Peach", "Apricot", "Pineapple"]
 var playersGuessedLetter = ""
 var wordToGuess = "madonna"
 wordToGuess = wordToGuess.split("")
 var answerArray = [];
 
+var selectAWord = () => {
+    var number = Math.floor(Math.random() * wordBank.length);
+    console.log(number);
+    wordToGuess = wordBank[number];
+    console.log(wordToGuess)
+    wordToGuess = wordToGuess.toLowerCase();
+    console.log(wordToGuess)
+}
+
 var fillTheAnswerArray = () => {
     for (var i = 0; i < wordToGuess.length; i++) {
-        answerArray.push("___");
+        answerArray.push("__");
     }
 }
 
@@ -66,11 +30,20 @@ var createTheAnswerHTML = () => {
 
 var updateSpecificLetter = (number, correctLetter) => {
     var span = document.getElementById(`${number}`);
-    span.innerHTML = correctLetter;
+    span.innerHTML = `    ${correctLetter}    `;
+}
+
+var checkForWin = () => {
+    wordToGuessJoined = wordToGuess.join("");
+    answerArrayJoined = answerArray.join("");
+    if (wordToGuessJoined === answerArrayJoined){
+        alert("You figured it out!")
+    }
 }
 
 $(document).ready(function() {
     // console.log("Document is ready")
+    // selectAWord();
     fillTheAnswerArray();
     createTheAnswerHTML();
     
@@ -87,8 +60,8 @@ document.onkeyup = function(e) {
             console.log(answerArray);
             updateSpecificLetter(i, typedLetter);
         }
-    
     }
+    checkForWin();
 
     
 }
